be4dcca68064e3b23f97c3b5af4caee601186f0a
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-08-17 23:37:44 +0200

    handle duplicate sheet names and sort files by date

--- main.js
+++ main.js
@@ -24,44 +24,93 @@ if(!args[0]) {
 const inputFolderPath = args[0];
 const outputFileName = args[1];
 
-const filesPath = path.resolve(inputFolderPath);
-const filesPaths = fs.readdirSync(filesPath).filter(file => {
-    return file.match('.csv');
-}).reverse();
+async function getFilesInDir(dirPath) {
+    return new Promise((resolve) => {
+        fs.readdir(dirPath, null, (err, filesPaths) => {
+            filesPaths = filesPaths.sort((a, b) => {
+                return fs.statSync(dirPath + "/" + a).mtime.getTime() - 
+                       fs.statSync(dirPath + "/" + b).mtime.getTime();
+            });
 
-console.log(filesPaths.length, 'csv files.');
+            console.log('Found', filesPaths.length, '.csv files:');
+            
+            for(let file of filesPaths) {
+                console.log(file);
+            }
 
-async function main() {
+            resolve(filesPaths);
+        });
+    })
+}
 
+async function convertFilesToWorkbook(dirPath, filesArray) {
     const workbook = XLSX.utils.book_new();
+
+    const sheetNames = [];
 
     const offset = SheetNamingScheme.offset;
     const categories = SheetNamingScheme.categories;
 
-    let index = 0;
-    for(let fileName of filesPaths) {
-        const filePath = path.resolve(filesPath, fileName);
+    let fileIndex = 0;
+    for(let fileName of filesArray) {
+        const filePath = path.resolve(dirPath, fileName);
         const csvString = fs.readFileSync(filePath).toString();
         const csvJsonData = await csv().fromString(csvString);
         const sheet = XLSX.utils.json_to_sheet(csvJsonData);
 
-        sheetName = fileName.replace('.csv', '');
-        if(!SheetNamingScheme.useFileNames) {
-            sheetName = `${(offset + Math.floor(index / 12))}_${categories[index % categories.length]}`;
+        const mkaeSheetName = index => {
+            let sheetName = fileName.replace('.csv', '').substring(0, 31);
+
+            if(!SheetNamingScheme.useFileNames) {
+                sheetName = `${(offset + Math.floor(fileIndex / 12))} ${categories[fileIndex % categories.length]}`;
+            }
+
+            if(index) {
+                sheetName = sheetName.substring(0, 28) + ' ' + index;
+            }
+
+            return sheetName;
         }
 
+        // check for duplicate sheet names
+        let sheetName = mkaeSheetName();
+        let sheetIndex = 0;
+
+        if(sheetNames.indexOf(sheetName) != -1) {
+            while(sheetNames.indexOf(sheetName) != -1) {
+                sheetName = mkaeSheetName(sheetIndex);
+                sheetIndex++;
+            }
+        }
+
+        sheetNames.push(sheetName);
+
+        // add sheet to book
         XLSX.utils.book_append_sheet(workbook, sheet, sheetName);
 
-        index++;
+        fileIndex++;
     }
     
-    console.log('Sheets:', Object.keys(workbook.Sheets));
+    console.log('Sheets:\n');
+    for(let sheet of Object.keys(workbook.Sheets)) {
+        console.log('  ' + sheet);
+    }
 
+    return workbook;
+}
+
+async function main() {
+    const dirPath = path.resolve(inputFolderPath);
+    const filesArray = await getFilesInDir(dirPath);
+
+    console.log('\nConverting to sheets...\n');
+    
+    const workbook = await convertFilesToWorkbook(inputFolderPath, filesArray);
+    
     const write_opts = {
         type: 'string',
         bookType: 'xlsx',
     }
-
     XLSX.writeFile(workbook, outputFileName, write_opts);
 }
 
